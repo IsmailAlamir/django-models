9edd3e51e7fd75193474e1e35d4eb332a964064f
IsmailAlamir <ismaeelalamir@gmail.com>
2022-12-03 15:21:31 +0300

    ADD Details

--- snacks/urls.py
+++ snacks/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import SnackListView
+from .views import SnackListView,Snack_Detail
 
 urlpatterns = [
     
     path('',SnackListView.as_view(), name="snacks"),
+    path('detail/<pk>',Snack_Detail.as_view(), name="detail"),
 
 ]
--- snacks/views.py
+++ snacks/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView,DetailView
 from .models import Snack
 
 # Create your views here.
-class SnackListView(ListView):
+class SnackListView (ListView):
 
     template_name='snack_list.html'
+    model=Snack
+    context_object_name= 'snack_item'
+
+class Snack_Detail(DetailView):
+    template_name='snack_detail.html'
     model=Snack
